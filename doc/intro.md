5a6ad907a4a016af4361c4aa093bd48c228ca676
Valtteri Pajunen <valtteri.pajunen@yle.fi>
2026-04-22 12:31:57 +0300

    All the things

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-snapshot-test
+
+TODO: write [great documentation](https://jacobian.org/writing/what-to-write/)
---
+++ test/clj_snapshot_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-snapshot-test.core-test
+  (:require [clojure.test :refer :all]
+            [clj-snapshot-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
